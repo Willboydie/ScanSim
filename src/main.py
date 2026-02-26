56e9a43b335c2a63e953e1db6edb289cde370535
Willboydie <williamteboyd@gmail.com>
2026-02-26 14:46:05 +0000

    removed main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import Terrain
-import ProjectionPath
-
-if __name__ == "__main__":
-    terrain = Terrain.Terrain()
-    projection_path = ProjectionPath.ProjectionPath()
-    terrain.slope_x_deg = 30
-    terrain.generate(seed=41, plot=True)
